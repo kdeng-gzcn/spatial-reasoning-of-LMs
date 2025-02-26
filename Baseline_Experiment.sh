2d589cbc10ebb9775ca23c624508f3126c8e2720
kdeng <kdeng.gzcn@gmail.com>
2025-02-26 00:58:26 +0000

    update bash

--- Baseline_Experiment.sh
+++ Baseline_Experiment.sh
@@ -2,6 +2,9 @@
 # module purge
 # module load baskerville
 
+module load GCC/12.3.0
+export LD_LIBRARY_PATH=/bask/apps/live/EL8-ice/software/GCCcore/12.3.0/lib64:$LD_LIBRARY_PATH
+
 # Activate Env
 source /bask/projects/j/jlxi8926-auto-sum/kdeng/anaconda3/etc/profile.d/conda.sh
 conda activate VLM
@@ -12,6 +15,6 @@ export HF_HOME=/bask/projects/j/jlxi8926-auto-sum/kdeng/SpatialVLM/cache/
 # Test
 python Experiments/Experiment0_IndividualVLM.py \
     --VLM "microsoft/Phi-3.5-vision-instruct" \
-    --data_path "./data/Rebuild_7_Scenes_1200_1738445186" \
-    --result_path "./Result/Individual VLM Experiment phi/" \
+    --data_path "./benchmark/Rebuild_7_Scenes_1739853799" \
+    --result_path "./Result/Individual VLM Experiment phi newbenchmark/" \
     --subset "phi"
