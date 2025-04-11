bfe7498765366fd442557ee2671cf68d09405df8
kdeng-gzcn <kdeng.gzcn@gmail.com>
2025-04-11 00:19:17 +0000

    update bash files

--- run_exp_multi_agents_reasoning.sh
+++ run_exp_multi_agents_reasoning.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
+source ~/spatial-reasoning-language-models/miniconda3/bin/activate spatial_reasoning_env
 
-module load GCC/12.3.0 # Load GCC for pandas
-export LD_LIBRARY_PATH=/bask/apps/live/EL8-ice/software/GCCcore/12.3.0/lib64:$LD_LIBRARY_PATH
-export HF_HOME=/bask/projects/j/jlxi8926-auto-sum/kdeng/SpatialVLM/cache/ # Transformers Cache Dir (Necessary)
+source .env
+huggingface-cli login --token "${HUGGINGFACE_TOKEN}"
 
-source /bask/projects/j/jlxi8926-auto-sum/kdeng/anaconda3/etc/profile.d/conda.sh
-conda activate VLM
+# RESULT_DIR="result/exp_ma_scale_up"
+PROMPT_TYPE="add-info-zero-shot"
+DATA_DIR="benchmark/RGBD_7_Scenes_Rebuilt"
+VLM_IMAGE_INPUT_TYPE="pair"
+MAX_LEN_OF_CONV=10
 
-# VLM_ID=microsoft/Phi-3.5-vision-instruct
-VLM_ID=Qwen/Qwen2.5-VL-7B-Instruct
-# LLM_ID=meta-llama/Meta-Llama-3-8B-Instruct
-LLM_ID=Qwen/Qwen2.5-7B-Instruct
-RESULT_DIR=result/exp_multi_agents_reasoning
-# PROMPT_TYPE=zero-shot
-PROMPT_TYPE=add-info-zero-shot
-
-python experiments/experiment_2_multi_agents.py \
-    --vlm_id ${VLM_ID} \
-    --llm_id ${LLM_ID} \
-    --data_dir "benchmark/RGBD_7_Scenes_Rebuilt" \
-    --result_dir ${RESULT_DIR} \
-    --vlm_image_input_type "pair" \
-    --split "phi" \
-    --is_shuffle \
-    --prompt_type ${PROMPT_TYPE} \
-    --max_len_of_conv 10 \
-    --is_vlm_keep_hisroty \
-    # --is_remove_trap_var \
+for VLM_ID in Qwen/Qwen2.5-VL-7B-Instruct gpt-4o; do
+    for LLM_ID in meta-llama/Llama-3.1-8B-Instruct Qwen/Qwen2.5-7B-Instruct deepseek-ai/DeepSeek-R1-Distill-Qwen-7B; do
+        for SPLIT in theta phi psi tx ty tz; do
+            echo "Running with VLM_ID: ${VLM_ID}, LLM_ID: ${LLM_ID} on split: ${SPLIT}"
+            RESULT_DIR=result/ma_big_table/$(basename "${VLM_ID}")/$(basename "${LLM_ID}")/${SPLIT}
+            python experiments/experiment_2_multi_agents.py \
+                --vlm_id "${VLM_ID}" \
+                --llm_id "${LLM_ID}" \
+                --data_dir "${DATA_DIR}" \
+                --result_dir "${RESULT_DIR}" \
+                --vlm_image_input_type "${VLM_IMAGE_INPUT_TYPE}" \
+                --split "${SPLIT}" \
+                --is_shuffle \
+                --prompt_type "${PROMPT_TYPE}" \
+                --max_len_of_conv "${MAX_LEN_OF_CONV}" \
+                --is_vlm_keep_hisroty \
+                --is_remove_trap_var
+        done
+    done
+done
--- run_exp_vlm_only_reasoning.sh
+++ run_exp_vlm_only_reasoning.sh
@@ -1,38 +1,36 @@
 #!/bin/bash
+source ~/spatial-reasoning-language-models/miniconda3/bin/activate spatial_reasoning_env
 
-# module purge
-# module load baskerville
-
-module load GCC/12.3.0
-export LD_LIBRARY_PATH=/bask/apps/live/EL8-ice/software/GCCcore/12.3.0/lib64:$LD_LIBRARY_PATH
-export HF_HOME=/bask/projects/j/jlxi8926-auto-sum/kdeng/SpatialVLM/cache/
-
-source /bask/projects/j/jlxi8926-auto-sum/kdeng/anaconda3/etc/profile.d/conda.sh
-conda activate VLM
+source .env
+huggingface-cli login --token "${HUGGINGFACE_TOKEN}"
+export HF_HOME=~/spatial-reasoning-language-models/spatial-reasoning-of-LMs/.cache/huggingface
 
 # VLM_ID=microsoft/Phi-3.5-vision-instruct
-# VLM_ID=Qwen/Qwen2.5-VL-7B-Instruct
-VLM_ID=gpt-4o
-SPLIT="phi"
-RESULT_DIR=result/test_new_code/${VLM_ID}_${SPLIT}
-PROMPT_TYPE=VoT-prompt
+VLM_ID=Qwen/Qwen2.5-VL-7B-Instruct
+# VLM_ID=gpt-4-turbo
+# SPLIT="tx"
+# PROMPT_TYPE=zero-shot
+# RESULT_DIR=result/vlm_only_big_table/${VLM_ID}/${PROMPT_TYPE}
 
-python experiments/experiment_1_individual_vlm.py \
-    --vlm_id ${VLM_ID} \
-    --data_dir "benchmark/RGBD_7_Scenes_Rebuilt" \
-    --result_dir ${RESULT_DIR} \
-    --split ${SPLIT} \
-    --is_shuffle \
-    --prompt_type ${PROMPT_TYPE}
+# python experiments/experiment_1_individual_vlm.py \
+#     --vlm_id ${VLM_ID} \
+#     --data_dir "benchmark/RGBD_7_Scenes_Rebuilt" \
+#     --result_dir ${RESULT_DIR} \
+#     --split ${SPLIT} \
+#     --is_shuffle \
+#     --prompt_type ${PROMPT_TYPE}
 
-# for PROMPT_TYPE in "zero-shot" "add-info-zero-shot" "CoT-zero-shot" "VoT-zero-shot"
-# do
-#     echo "Running experiment with prompt_type=${PROMPT_TYPE}"
-#     python experiments/experiment_1_individual_vlm.py \
-#         --vlm_id ${VLM_ID} \
-#         --data_dir "benchmark/RGBD_7_Scenes_Rebuilt" \
-#         --result_dir ${RESULT_DIR} \
-#         --split "phi" \
-#         --is_shuffle \
-#         --prompt_type ${PROMPT_TYPE}
-# done
+for PROMPT_TYPE in "zero-shot" "add-info-zero-shot" "CoT-zero-shot" "VoT-zero-shot"; do
+    for SPLIT in "theta" "phi" "psi" "tx" "ty" "tz"; do
+        echo "Running experiment with prompt_type=${PROMPT_TYPE} and split=${SPLIT}"
+        RESULT_DIR=result/vlm_only_big_table/${VLM_ID}/${PROMPT_TYPE}/${SPLIT}
+        mkdir -p ${RESULT_DIR}
+        python experiments/experiment_1_individual_vlm.py \
+            --vlm_id ${VLM_ID} \
+            --data_dir "benchmark/RGBD_7_Scenes_Rebuilt" \
+            --result_dir ${RESULT_DIR} \
+            --split ${SPLIT} \
+            --is_shuffle \
+            --prompt_type ${PROMPT_TYPE}
+    done
+done
